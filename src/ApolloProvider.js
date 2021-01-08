743654c434ab448cafd09a659b01767f9634981c
RubinVelcani <60612243+RubinVelcani@users.noreply.github.com>
2021-01-08 16:12:57 +0100

    added proxy

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from '@apollo/react-hooks'
 import { setContext } from 'apollo-link-context'
 
 const httpLink = createHttpLink({
-    uri: 'https://git.heroku.com/shielded-harbor-34959.git'
+    uri: 'https://tranquil-crag-47571.herokuapp.com/'
 })
 
 const authLink = setContext(() => {
