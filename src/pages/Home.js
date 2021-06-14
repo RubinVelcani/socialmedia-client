8c8d970fd01eb1aae87d6a7857c7bde7cdf768b2
RubinVelcani <60612243+RubinVelcani@users.noreply.github.com>
2021-06-14 12:08:22 +0200

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -18,12 +18,12 @@ function Home() {
     <Grid columns={3}>
       <Grid.Row className="page-title">
         <h1>Recent Posts</h1>
-          {user && (
+      </Grid.Row>
+{user && (
                       <Grid.Column mobile={16} tablet={8} largeScreen={5} style={{ marginBottom: 40 }}>
                       <PostForm />
                       </Grid.Column>
                     )}
-      </Grid.Row>
       <Grid.Row>
         {loading ? (
           <h1>Loading posts..</h1>
